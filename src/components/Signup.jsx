519d7bc5681df5945be5cdb7f2c4a9d808ea5a61
Abdur Rehman <94692937+AbdurRehman6@users.noreply.github.com>
2023-03-20 11:29:20 +0530

    Add files via upload

---
+++ src/components/Signup.jsx
@@ -0,0 +1,65 @@
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  Link,
+  VStack,
+  Text,
+  Avatar,
+} from '@chakra-ui/react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Signup = () => {
+  return (
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+      <form>
+        <VStack
+          alignItems={'stretch'}
+          spacing={'4'}
+          w={['full', '96']}
+          m={'auto'}
+          my={'16'}
+        >
+          <Heading alignSelf={'center'}>VIDEO HUB</Heading>
+          <Avatar alignSelf={'center'} boxSize={'32'} />
+
+          <Input
+            placeholder={'Name'}
+            type={'text'}
+            required
+            focusBorderColor={'purple.500'}
+          ></Input>
+
+          <Input
+            placeholder={'Enter your email'}
+            type={'email'}
+            required
+            focusBorderColor={'purple.500'}
+          ></Input>
+
+          <Input
+            placeholder={'Create Password'}
+            type={'password'}
+            required
+            focusBorderColor={'purple.500'}
+          ></Input>
+
+          <Button variant={'link'} alignSelf={'flex-end'}>
+            <Link to={'/forgotpassword'}>Forgot Password</Link>
+          </Button>
+
+          <Button colorScheme={'purple'} type={'submit'}>
+            Sign Up
+          </Button>
+
+          <Text textAlign={'right'}>Already Signed Up?{' '}</Text>
+          <Link to={'/login'}>Log In </Link>
+        </VStack>
+      </form>
+    </Container>
+  );
+};
+
+export default Signup;
